7395cebdea6abc4a9576857008c88837d37717f4
Muhammad Abubakar Siddique <abubakarflutter@gmail.com>
2025-06-24 19:16:15 +0500

    admin bottom nav

---
+++ lib/core/app_resources/app_assets.dart
@@ -0,0 +1,19 @@
+class AppAssets {
+  ///------- App Images
+  ///------- App Icons
+  static  const navHomeOutlineIcon = 'assets/icons/bottom_nav_icons/home-outline.svg';
+  static  const navHomeFilledIcon = 'assets/icons/bottom_nav_icons/home-filled.svg';
+  static  const navVehicleOutlineIcon = 'assets/icons/bottom_nav_icons/vehicle-outline.svg';
+  static  const navVehicleFilledIcon = 'assets/icons/bottom_nav_icons/vehicle-filled.svg';
+  static  const navOffersFilledIcon = 'assets/icons/bottom_nav_icons/offer-filled.svg';
+  static  const navOffersOutlineIcon = 'assets/icons/bottom_nav_icons/offer-outline.svg';
+  static  const navJobsOutlineIcon = 'assets/icons/bottom_nav_icons/jobs-outline.svg';
+  static  const navJobsFilledIcon = 'assets/icons/bottom_nav_icons/jobs-filled.svg';
+  static  const navMoreFilledIcon = 'assets/icons/bottom_nav_icons/more-filled.svg';
+  static  const navMoreOutlineIcon = 'assets/icons/bottom_nav_icons/more-outline.svg';
+
+
+
+
+
+}
--- lib/features/admin/bottom_nav/views/bottom_nav_view.dart
+++ lib/features/admin/bottom_nav/views/bottom_nav_view.dart
@@ -1,19 +1,195 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hjulfix_new/core/app_resources/app_assets.dart';
 
-import '../../../shared/auth/repo/auth_repo.dart';
+import '../../../../core/app_resources/app_colors.dart';
 
-class BottomNavView extends ConsumerWidget {
+class BottomNavView extends ConsumerStatefulWidget {
   const BottomNavView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final authRepo = ref.read(authRepositoryProvider);
-    final isLoggedIn = authRepo.isLoggedIn();
+  ConsumerState createState() => _BottomNavViewState();
+}
 
-    return Scaffold(
-      body: Center(
-        child: Text("Logged In \n ${isLoggedIn ? "Logged In" : "Not Logged In"}",),
+class _BottomNavViewState extends ConsumerState<BottomNavView> {
+
+  int index = 0;
+  List<Widget> screens = const [
+    Center(
+      child: Text('Dashboard'),
+    ),
+    Center(
+      child: Text('Vehicles'),
+    ),
+    Center(
+      child: Text('Offers'),
+    ),
+    Center(
+      child: Text('Jobs'),
+    ),
+    Center(
+      child: Text('More'),
+    ),
+  ];
+
+  Future<bool> onWillPop() async {
+    if (index != 0) {
+      // logic.changeCurrentPage(0);
+      return false;
+    } else {
+      // final value = await showExitDialog(context);
+      // if (value != null) {
+      //   return value;
+      // } else {
+      //   return value;
+      // }
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: onWillPop,
+      child: Scaffold(
+        body: IndexedStack(
+          index: index,
+          children: screens,
+        ),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            navigationBarTheme: NavigationBarThemeData(
+              backgroundColor: Colors.white,
+              labelTextStyle: WidgetStateProperty.resolveWith((states) {
+                if (states.contains(WidgetState.selected)) {
+                  return TextStyle(
+                    color: AppColors.primary,
+                    fontFamily: "Montserrat",
+                    fontSize: 10.5.sp,
+                      fontWeight: FontWeight.w600
+
+                  );
+                }
+                return TextStyle(
+                  color: Color(0xff8F959E),
+                  fontFamily: "Montserrat",
+                  fontSize: 10.25.sp,
+                  fontWeight: FontWeight.w500
+                );
+              }),
+            ),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset:
+                  Offset(0, -5), // Negative y-offset for upper shadow
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: NavigationBar(
+              backgroundColor: Colors.white,
+              onDestinationSelected: (value) {
+                HapticFeedback.lightImpact();
+                setState(() {
+                  index = value;
+                });
+              },
+              selectedIndex: index,
+              labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+              // elevation: 100,
+              height: 65.h, // Adjust this value as needed
+              indicatorColor: AppColors.primary.withOpacity(.2),
+              animationDuration: const Duration(milliseconds: 800),
+              // surfaceTintColor: AppColors.primaryOrange.withOpacity(.2),
+              overlayColor: WidgetStateProperty.resolveWith((states) {
+                if (states.contains(WidgetState.selected)) {
+                  return AppColors.primary.withOpacity(.2);
+                }
+                return AppColors.primary.withOpacity(.2);
+              }),
+              destinations: [
+                _buildNavBarDestination(
+                  icon: AppAssets.navHomeOutlineIcon,
+                  activeIcon: AppAssets.navHomeFilledIcon,
+                  label: "Home",
+                  itemIndex: 0,
+                ),
+                _buildNavBarDestination(
+                  icon: AppAssets.navVehicleOutlineIcon,
+                  activeIcon: AppAssets.navVehicleFilledIcon,
+                  label: "Vehicles",
+                  itemIndex: 1,
+                  outlineIconSize: 27,
+                  filledIconSize: 20
+                ),
+                _buildNavBarDestination(
+                  icon: AppAssets.navOffersOutlineIcon,
+                  activeIcon: AppAssets.navOffersFilledIcon,
+                  label: "Offers",
+                  itemIndex: 2,
+                  isCart: true,
+                ),
+                _buildNavBarDestination(
+                  icon: AppAssets.navJobsOutlineIcon,
+                  activeIcon: AppAssets.navJobsFilledIcon,
+                  label: "Jobs",
+                  itemIndex: 3,
+                ),
+                _buildNavBarDestination(
+                  icon: AppAssets.navMoreOutlineIcon,
+                  activeIcon: AppAssets.navMoreFilledIcon,
+                  label: "More",
+                  itemIndex: 4,
+                  filledIconSize: 20
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  NavigationDestination _buildNavBarDestination({
+    required String icon,
+    required String activeIcon,
+    required String label,
+    required int itemIndex,
+    bool isCart = false,
+    int filledIconSize = 22,
+    int outlineIconSize = 22
+  }) {
+    return NavigationDestination(
+      label: label,
+      tooltip: label,
+      icon: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SvgPicture.asset(
+            icon,
+            height: outlineIconSize.h,
+          )
+        ],
+      ),
+      selectedIcon: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SvgPicture.asset(
+            activeIcon,
+            color: AppColors.primary,
+            height: filledIconSize.h,
+          ),
+        ],
       ),
     );
   }
